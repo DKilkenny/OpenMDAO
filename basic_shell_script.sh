8ca6d7a6127f0ba32dfbb838bf590a0acd4e83c2
Danny Kilkenny <danny.kilkenny18@gmail.com>
2020-02-28 13:19:17 -0500

    Changed sh to bash, started creating conda env

--- basic_shell_script.sh
+++ basic_shell_script.sh
@@ -1,15 +1,22 @@
-#! /bin/sh
+#!/bin/bash
 
 PY=3.6
-
-
+NUMPY=1.16
+SCIPY=1.2
+PETSc=3.10.1
 
 echo "Building python environment";
 echo "Installing conda";
 echo "Downloading miniconda";
-sudo apt-get install wget;
-wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O miniconda.sh;
+wget "https://repo.anaconda.com/miniconda/Miniconda${PY:0:1}-latest-Linux-x86_64.sh" -O miniconda.sh;
 chmod +x miniconda.sh;
-echo "  > Installing miniconda";
+echo "Installing miniconda";
 ./miniconda.sh -b  -p $HOME/miniconda;
 export PATH=$HOME/miniconda/bin:$PATH;
+
+echo " >> Creating conda environment";
+conda create --yes -n PY$PY python=$PY;
+source $HOME/miniconda/bin/activate PY$PY;
+
+echo " >> Installing non-pure Python dependencies from conda";
+conda install --yes numpy=$NUMPY scipy=$SCIPY cython swig;
